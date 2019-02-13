47f8fc6274849395ccfaed91076292f685a8df4b
rootglobal <rootglobal@qq.com>
2019-02-13 10:04:29 +0800

    master...

--- src/main/java/com/git/Happy.java
+++ src/main/java/com/git/Happy.java
@@ -2,6 +2,7 @@ package com.git;
 
 public class Happy {
 	public static void main(String[] args) {
+		System.out.println("master..");
 		System.out.println("happy right...");
 		System.out.println("happy left...");
 		System.out.println("hot fix ...");
